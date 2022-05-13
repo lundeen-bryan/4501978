2a8d61f28709304444607cedc001feb836e3927b
lundeen-bryan <lundeen.bryan@gmail.com>
2022-05-13 10:06:20 -0700

    Finished update rows script section

---
+++ SQL_queries/update_rows.sql
@@ -0,0 +1,43 @@
+USE [TutorialDB]
+;
+
+/* #region Select queries  */
+--/*
+SELECT [Second] = A.[Second_City]
+FROM [dbo].[Countries] AS A
+WHERE 1=1
+;
+--*/
+/* #endregion */
+
+/* #region  Update queries */
+/*
+UPDATE [dbo].[Countries] SET Second_City ='Berat' WHERE [dbo].[Countries].[Country_ID] = 2;
+UPDATE [dbo].[Countries] SET Second_City ='Oran' WHERE [dbo].[Countries].[Country_ID] = 3;
+UPDATE [dbo].[Countries] SET Second_City ='Encamp' WHERE [dbo].[Countries].[Country_ID] = 4;
+UPDATE [dbo].[Countries] SET Second_City ='Bungo' WHERE [dbo].[Countries].[Country_ID] = 5;
+UPDATE [dbo].[Countries] SET Second_City ='All Saints' WHERE [dbo].[Countries].[Country_ID] = 6;
+UPDATE [dbo].[Countries] SET Second_City ='La Plata' WHERE [dbo].[Countries].[Country_ID] = 7;
+UPDATE [dbo].[Countries] SET Second_City ='Sevan' WHERE [dbo].[Countries].[Country_ID] = 8;
+UPDATE [dbo].[Countries] SET Second_City ='Tasmania' WHERE [dbo].[Countries].[Country_ID] = 9;
+UPDATE [dbo].[Countries] SET Second_City ='Graz' WHERE [dbo].[Countries].[Country_ID] = 10;
+UPDATE [dbo].[Countries] SET Second_City ='Astara' WHERE [dbo].[Countries].[Country_ID] = 11;
+UPDATE [dbo].[Countries] SET Second_City ='Clarence Town' WHERE [dbo].[Countries].[Country_ID] = 12;
+UPDATE [dbo].[Countries] SET Second_City ='Mahooz' WHERE [dbo].[Countries].[Country_ID] = 13;
+UPDATE [dbo].[Countries] SET Second_City ='Khulna' WHERE [dbo].[Countries].[Country_ID] = 14;
+UPDATE [dbo].[Countries] SET Second_City ='Saint Michael' WHERE [dbo].[Countries].[Country_ID] = 15;
+UPDATE [dbo].[Countries] SET Second_City ='Orsha' WHERE [dbo].[Countries].[Country_ID] = 16;
+UPDATE [dbo].[Countries] SET Second_City ='Antwerp' WHERE [dbo].[Countries].[Country_ID] = 17;
+UPDATE [dbo].[Countries] SET Second_City ='Dangriga' WHERE [dbo].[Countries].[Country_ID] = 18;
+UPDATE [dbo].[Countries] SET Second_City ='Kandi' WHERE [dbo].[Countries].[Country_ID] = 19;
+UPDATE [dbo].[Countries] SET Second_City ='Mongar' WHERE [dbo].[Countries].[Country_ID] = 20;
+UPDATE [dbo].[Countries] SET Second_City ='Montero' WHERE [dbo].[Countries].[Country_ID] = 21;
+UPDATE [dbo].[Countries] SET Second_City ='Jajce' WHERE [dbo].[Countries].[Country_ID] = 22;
+UPDATE [dbo].[Countries] SET Second_City ='Kanye' WHERE [dbo].[Countries].[Country_ID] = 23;
+UPDATE [dbo].[Countries] SET Second_City ='Sao Paulo' WHERE [dbo].[Countries].[Country_ID] = 24;
+UPDATE [dbo].[Countries] SET Second_City ='Sukang' WHERE [dbo].[Countries].[Country_ID] = 25;
+UPDATE [dbo].[Countries] SET Second_City ='Pleven' WHERE [dbo].[Countries].[Country_ID] = 26;
+UPDATE [dbo].[Countries] SET Second_City ='Boromo' WHERE [dbo].[Countries].[Country_ID] = 27;
+UPDATE [dbo].[Countries] SET Second_City ='Ngozi' WHERE [dbo].[Countries].[Country_ID] = 28;
+--*/
+/* #endregion */
